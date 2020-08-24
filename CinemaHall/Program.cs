707ff1c38e0806ad35ef91d5a700a8ded4ed07d5
Anastasia <anastasila.mik@yandex.ru>
2020-08-24 08:41:26 +0300

    Edit Program

--- CinemaHall/Program.cs
+++ CinemaHall/Program.cs
@@ -7,46 +7,28 @@ namespace CinemaHall
     {
         static void Main(string[] args)
         {
-            int cinemaWorkTime = 500;
+            int cinemaWorkTime = 600;
 
-            int hallNumber = 10;
+            Console.WriteLine("Введите количество залов в кинотеатре:");
+            int hallNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите количество фильмов в прокате:");
+            int filmNumber = Convert.ToInt32(Console.ReadLine());
 
             List<Film> films = new List<Film>();
+            int count = 1;
+            while (count <= filmNumber)
+            {
+                Console.WriteLine($"Введите название фильма {count}:");
+                string filmName = Convert.ToString(Console.ReadLine());
 
-            //films.Add(new Film(1, "Человек паук", 60));
-            //films.Add(new Film(2, "Властелин колец", 30));
-            //films.Add(new Film(3, "Титаник", 590));
-            //films.Add(new Film(4, "Война и мир", 90));
-            //films.Add(new Film(5, "Война и мир 2", 300));
-            //films.Add(new Film(6, "Война и мир 3", 575));
+                Console.WriteLine($"Введите продолжительность фильма в минутах:");
+                int filmDuration = Convert.ToInt32(Console.ReadLine());
 
-            //films.Add(new Film(1, "Человек паук", 550));
-            //films.Add(new Film(2, "Властелин колец", 580));
-            //films.Add(new Film(3, "Титаник", 590));
-            //films.Add(new Film(4, "Война и мир", 300));
-            //films.Add(new Film(5, "Война и мир 2", 300));
-            //films.Add(new Film(6, "Война и мир 3", 575));
+                films.Add(new Film(count, filmName, filmDuration));
 
-            //Console.WriteLine("Введите количество залов в кинотеатре:");
-            //int hallNumber = Convert.ToInt32(Console.ReadLine());
-
-            //Console.WriteLine("Введите количество фильмов в прокате:");
-            //int filmNumber = Convert.ToInt32(Console.ReadLine());
-
-            //List<Film> films = new List<Film>();
-            //int count = 1;
-            //while (count < filmNumber)
-            //{
-            //    Console.WriteLine($"Введите название фильма {count}:");
-            //    string filmName = Convert.ToString(Console.ReadLine());
-
-            //    Console.WriteLine($"Введите продолжительность фильма в минутах:");
-            //    int filmDuration = Convert.ToInt32(Console.ReadLine());
-
-            //    films.Add(new Film(count, filmName, filmDuration));
-
-            //    count++;
-            //}
+                count++;
+            }
 
             Cinema cinema = new Cinema(hallNumber, cinemaWorkTime, films);
             Schedule schedule = new Schedule();
